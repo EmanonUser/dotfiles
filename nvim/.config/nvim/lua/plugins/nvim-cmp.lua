6fde45b0f1f19eaded78c3f2fa49523c59755b84
Emanon <moemanon@pm.me>
2023-09-24 18:17:40 +0200

    remove unused lines and format

--- nvim/.config/nvim/lua/plugins/nvim-cmp.lua
+++ nvim/.config/nvim/lua/plugins/nvim-cmp.lua
@@ -1,53 +1,47 @@
 return {
-  "hrsh7th/nvim-cmp",
-  event = "InsertEnter",
-  dependencies = {
-    "hrsh7th/cmp-buffer",
-    "hrsh7th/cmp-path",
-	"hrsh7th/cmp-nvim-lsp",
-    "L3MON4D3/LuaSnip",
-    "saadparwaiz1/cmp_luasnip",
-    "rafamadriz/friendly-snippets",
-  },
-  config = function()
-	local capabilities = require("cmp_nvim_lsp").default_capabilities()
-	local lspconfig = require('lspconfig')
-	local servers = { 'clangd', 'rust_analyzer', 'pyright', 'tsserver', "ansible-language-server" }
-	for _, lsp in ipairs(servers) do
-	  lspconfig[lsp].setup {
-	    on_attach = my_custom_on_attach,
-	    capabilities = capabilities,
-	  }
-	end
-    local cmp = require("cmp")
+	"hrsh7th/nvim-cmp",
+	event = "InsertEnter",
+	dependencies = {
+		"hrsh7th/cmp-buffer",
+		"hrsh7th/cmp-path",
+		"hrsh7th/cmp-nvim-lsp",
+		"L3MON4D3/LuaSnip",
+		"saadparwaiz1/cmp_luasnip",
+		"rafamadriz/friendly-snippets",
+	},
+	config = function()
+		local capabilities = require("cmp_nvim_lsp").default_capabilities()
+		local lspconfig = require('lspconfig')
+		local servers = { 'clangd', 'rust_analyzer', 'pyright', 'tsserver', "ansible-language-server" }
+		local cmp = require("cmp")
 
-    local luasnip = require("luasnip")
+		local luasnip = require("luasnip")
 
-    cmp.setup({
-      completion = {
-        completeopt = "menu,menuone,preview,noselect",
-      },
+		cmp.setup({
+			completion = {
+				completeopt = "menu,menuone,preview,noselect",
+			},
 
-      snippet = {
-        expand = function(args)
-          luasnip.lsp_expand(args.body)
-        end,
-      },
+			snippet = {
+				expand = function(args)
+					luasnip.lsp_expand(args.body)
+				end,
+			},
 
-      mapping = cmp.mapping.preset.insert({
-        ["<C-l>"] = cmp.mapping.select_next_item(),
-        ["<C-m>"] = cmp.mapping.select_prev_item(),
-        ["<C-space>"] = cmp.mapping.complete(),
-        ["<C-y>"] = cmp.mapping.confirm({ select = true }),
-        ["<C-e>"] = cmp.mapping.abort(),
-      }),
+			mapping = cmp.mapping.preset.insert({
+				["<C-l>"] = cmp.mapping.select_next_item(),
+				["<C-m>"] = cmp.mapping.select_prev_item(),
+				["<C-space>"] = cmp.mapping.complete(),
+				["<C-y>"] = cmp.mapping.confirm({ select = true }),
+				["<C-e>"] = cmp.mapping.abort(),
+			}),
 
-      sources = cmp.config.sources({
-        { name = "nvim_lsp" },
-        { name = "luasnip" },
-        { name = "buffer" },
-        { name = "path" },
-      }),
-    })
-  end,
+			sources = cmp.config.sources({
+				{ name = "nvim_lsp" },
+				{ name = "luasnip" },
+				{ name = "buffer" },
+				{ name = "path" },
+			}),
+		})
+	end,
 }
