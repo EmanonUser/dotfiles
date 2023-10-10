854052d69fb9cf29abd67b81d69523fcf02d679b
Emanon <moemanon@pm.me>
2023-10-10 06:15:52 +0200

    add yaml lsp

--- nvim/.config/nvim/lua/plugins/nvim-lsp-config.lua
+++ nvim/.config/nvim/lua/plugins/nvim-lsp-config.lua
@@ -4,10 +4,18 @@ return {
   config = function()
     -- Setup language servers.
     local lspconfig = require('lspconfig')
+    local lsp_defaults = lspconfig.util.default_config
+
+    lsp_defaults.capabilities = vim.tbl_deep_extend(
+      'force',
+      lsp_defaults.capabilities,
+      require('cmp_nvim_lsp').default_capabilities()
+    )
     lspconfig.pyright.setup {}
     lspconfig.tsserver.setup {}
     lspconfig.taplo.setup {}
     lspconfig.dockerls.setup {}
+    lspconfig.yamlls.setup {}
     lspconfig.lua_ls.setup {
       settings = {
         Lua = {
