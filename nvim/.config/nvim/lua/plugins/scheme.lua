6d74bf42b1e2ed877fad022c4726efb4cb5e29f2
Emanon <moemanon@pm.me>
2025-04-10 22:22:14 +0200

    feat: add new themes

--- nvim/.config/nvim/lua/plugins/scheme.lua
+++ nvim/.config/nvim/lua/plugins/scheme.lua
@@ -4,7 +4,28 @@ return {
     lazy = false,
     priority = 1000,
     config = function()
-      vim.cmd([[colorscheme cyberdream]])
+      vim.cmd.colorscheme("cyberdream")
     end,
+  },
+
+
+  {
+    "rebelot/kanagawa.nvim",
+    lazy = false,
+    priority = 1000,
+    config = function()
+      --vim.cmd.colorscheme('kanagawa')
+    end
+  },
+
+  {
+    "sainnhe/everforest",
+    lazy = false,
+    priority = 1000,
+    config = function()
+      vim.g.everforest_enable_italic = false
+      vim.g.everforest_background = "hard"
+      --vim.cmd.colorscheme("everforest")
+    end
   }
 }
