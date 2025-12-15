da7dd053fd1d7dc6dd5d563e94a23d02345e59c6
Emanon <moemanon@pm.me>
2025-12-15 23:49:43 +0100

    feat: small neovim update

--- nvim/.config/nvim/lua/plugins/lsp.lua
+++ nvim/.config/nvim/lua/plugins/lsp.lua
@@ -1,8 +1,8 @@
 return {
   "neovim/nvim-lspconfig",
+  dependencies = { 'saghen/blink.cmp' },
   priority = 1,
   config = function()
-    local lspconfig = require('lspconfig')
     local border = {
       { "┌", "FloatBorder" },
       { "─", "FloatBorder" },
@@ -20,48 +20,5 @@ return {
       opts.border = opts.border or border
       return orig_util_open_floating_preview(contents, syntax, opts, ...)
     end
-
-    vim.api.nvim_create_autocmd('LspAttach', {
-      group = vim.api.nvim_create_augroup('UserLspConfig', {}),
-      callback = function(event)
-        local map = function(keys, func, desc, mode)
-          mode = mode or 'n'
-          vim.keymap.set(mode, keys, func, { buffer = event.buf, desc = 'LSP: ' .. desc })
-        end
-        map('gd', require('telescope.builtin').lsp_definitions, '[G]oto [D]efinition')
-        map('gr', require('telescope.builtin').lsp_references, '[G]oto [R]eferences')
-        map('gi', require('telescope.builtin').lsp_implementations, '[G]oto [I]mplementation')
-        map('<leader>D', require('telescope.builtin').lsp_type_definitions, 'Type [D]efinition')
-        map('<leader>ds', require('telescope.builtin').lsp_document_symbols, '[D]ocument [S]ymbols')
-        map('<leader>ws', require('telescope.builtin').lsp_dynamic_workspace_symbols, '[W]orkspace [S]ymbols')
-        map('<leader>rn', vim.lsp.buf.rename, '[R]e[n]ame')
-        map('<leader>ca', vim.lsp.buf.code_action, '[C]ode [A]ction', { 'n', 'x' })
-        map('gD', vim.lsp.buf.declaration, '[G]oto [D]eclaration')
-      end,
-    })
-
-    local capabilities = require('blink.cmp').get_lsp_capabilities()
-    lspconfig.pyright.setup { capabilities = capabilities }
-    lspconfig.taplo.setup { capabilities = capabilities }
-    lspconfig.dockerls.setup { capabilities = capabilities }
-    lspconfig.yamlls.setup { capabilities = capabilities }
-    lspconfig.nil_ls.setup { capabilities = capabilities }
-    lspconfig.lua_ls.setup {
-      capabilities = capabilities,
-      settings = {
-        Lua = {
-          diagnostics = {
-            globals = { 'vim' },
-          },
-        },
-      },
-    }
-    lspconfig.ansiblels.setup { capabilities = capabilities }
-    lspconfig.rust_analyzer.setup {
-      capabilities = capabilities,
-      settings = {
-        ['rust-analyzer'] = {},
-      },
-    }
-  end,
+  end
 }
