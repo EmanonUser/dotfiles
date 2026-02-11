b502672532b61e8612c179402c609d7a57053afa
Emanon <moemanon@pm.me>
2026-02-11 14:48:57 +0100

    feat: add nix dev env

---
+++ flake.nix
@@ -0,0 +1,22 @@
+{
+  description = "dotfiles development environment";
+  inputs.nixpkgs.url = "github:nixos/nixpkgs";
+  inputs.flake-utils.url = "github:numtide/flake-utils";
+
+  outputs = {
+    nixpkgs,
+    flake-utils,
+    ...
+  }:
+    flake-utils.lib.eachDefaultSystem (system: let
+      pkgs = import nixpkgs {inherit system;};
+    in {
+      devShells.default = pkgs.mkShell {
+        packages = [
+          pkgs.lua-language-server
+          pkgs.taplo
+          pkgs.nixd
+        ];
+      };
+    });
+}
