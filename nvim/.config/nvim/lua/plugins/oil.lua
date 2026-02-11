3d935520f47a8a284a06c3349058c94993289ca3
Emanon <moemanon@pm.me>
2026-02-11 15:39:53 +0100

    fix: show hidden files

--- nvim/.config/nvim/lua/plugins/oil.lua
+++ nvim/.config/nvim/lua/plugins/oil.lua
@@ -1,8 +1,13 @@
 return {
   'stevearc/oil.nvim',
-  opts = {
-    vim.keymap.set("n", "<leader>pv", vim.cmd.Oil)
-  },
-  dependencies = { "nvim-tree/nvim-web-devicons" },
   lazy = false,
+  dependencies = { "nvim-tree/nvim-web-devicons" },
+  config = function()
+    require("oil").setup({
+      view_options = {
+        show_hidden = true,
+      },
+    })
+    vim.keymap.set("n", "<leader>pv", "<CMD>Oil<CR>", { desc = "Open parent directory" })
+  end,
 }
